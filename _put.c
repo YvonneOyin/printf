405d6e50e8b5702e2dca4c90f3beb5c9416b7142
YvonneOyin <olufemioyindamola37@gmail.com>
2023-10-17 18:01:24 +0100

    A very simplified version of puts that prints a string to the standard output and returns number of character printed.

---
+++ _put.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * put - prints a string
+ *
+ * @str: string to be considered
+ *
+ * Return: number of character printed
+ */
+int _put(char *str)
+{
+	register int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
